e34f872eb6a432138fd3366f6b0d4e780025eab1
Victor Grachev <v.grachev@cft.ru>
2021-10-03 16:10:49 +0700

    Рефакторинг передачи props

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,17 +1,14 @@
 import './style/Column.css';
 
-function Column(props) {
-  const rows = props.rows ? props.rows : [];
-  const title = props.title;
-
+function Column({ title, rows = [] }) {
   return (
     <div className="column">
       <div className="row">
         <span>{title}</span>
       </div>
       {rows.map((row, indx) => (
-        <div className="row" key={indx}>
-          <span>{row}</span>
+        <div className={`row ${row.select ? 'selected' : ''}`} key={indx}>
+          <span>{row.value}</span>
         </div>
       ))}
     </div>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,13 +1,42 @@
 import './style/Table.css';
 import Column from './Column';
 
-function Table(props) {
-  const keys = Object.keys(props.colums);
+function Table({ column }) {
+  const keys = Object.keys(column);
+  const markets = keys.filter(
+    (key) => key !== 'Pair name/market' && column[key]
+  );
+  const selectRow = [];
+
+  markets.forEach((marketKey, indexMarket, arrMarketIteration) => {
+    column[marketKey].forEach((marketValue, indexMarketValue) => {
+      marketValue.select = false;
+
+      const compareValueRow = [marketValue.value];
+
+      arrMarketIteration.forEach((keyMarketIteration) => {
+        if (keyMarketIteration !== marketKey) {
+          compareValueRow.push(
+            column[keyMarketIteration][indexMarketValue].value
+          );
+        }
+      });
+      const minRowValue = Math.min(...compareValueRow);
+
+      if (minRowValue === marketValue.value) {
+        selectRow.push({ key: marketKey, indx: indexMarketValue });
+      }
+    });
+  });
+
+  selectRow.forEach((value) => {
+    column[value.key][value.indx].select = true;
+  });
 
   return (
     <div className="table">
       {keys.map((key, indx) => (
-        <Column title={key} rows={props.colums[key]} key={indx} />
+        <Column title={key} rows={column[key]} key={indx} />
       ))}
     </div>
   );
