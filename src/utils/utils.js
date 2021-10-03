0dcb02c4c01fc27d7d5eae464d6ef903b8533127
Victor Grachev <v.grachev@cft.ru>
2021-10-03 16:09:43 +0700

    Разделение логики расчета и загрузки

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,14 +1,21 @@
-export async function fetchCurPair(url, pairs) {
-  try {
-    const response = await fetch(`http://localhost:8000/api/v1/${url}`);
-    const body = await response.json();
-    const curPairs = calcCurPair(body.base, body.rates);
-    return pairs.map((pair) =>
-      curPairs[pair] ? Math.round(curPairs[pair] * 1000) / 1000 : 0
-    );
-  } catch (err) {
-    console.log(err);
-  }
+export async function fetchCurInfo(url, poll) {
+  const response = await fetch(
+    `${process.env.REACT_APP_URL_BACKEND}${url}${poll ? '/poll' : ''}`
+  );
+  return response;
+}
+
+export function getCurPairs(base, rates, pairs) {
+  const curPairs = calcCurPair(base, rates);
+  return pairs.map((pair) => {
+    if (curPairs[pair.value]) {
+      return {
+        value: Math.round(curPairs[pair.value] * 1000) / 1000,
+      };
+    } else {
+      return { value: 0 };
+    }
+  });
 }
 
 function calcCurPair(base, rates) {
